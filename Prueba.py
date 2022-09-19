664892996c6ccb6c036c1cf3cf627728ef46cb72
Jesus <jesus.santana@uaq.mx>
2022-09-19 15:54:29 -0500

    lo que quiera

--- Prueba.py
+++ Prueba.py
@@ -1,5 +1,6 @@
 import nidaqmx
 import time 
+from numpy import arange
 from openpyxl import Workbook
 from tkinter import *
 from threading import Thread
@@ -9,6 +10,7 @@ class Testing():
     def __init__(self,analog=None):
         self.task = nidaqmx.Task()
         self.task2 = nidaqmx.Task()
+        self.task3 = nidaqmx.Task()
         self.value = [0,0,0,0]
         self.hilo = None
         self.runS = False
@@ -24,23 +26,26 @@ class Testing():
             self.task.ai_channels.add_ai_voltage_chan('Dev1/ai1',min_val= 0, max_val = 5)
             self.task.ai_channels.add_ai_voltage_chan('Dev1/ai2',min_val= 0, max_val = 5)
             self.task.ai_channels.add_ai_voltage_chan('Dev1/ai3',min_val= 0, max_val = 5)
-            #self.task2.ao_channels.add_ao_voltage_chan('Dev1/ao0','mychanel',0,6)
-            self.task2.ao_channels.add_ao_voltage_chan('Dev1/ao1','mychanel',0,6)
+            self.task3.ao_channels.add_ao_voltage_chan('Dev1/ao0','mychanel',0,3.3)
+            self.task2.ao_channels.add_ao_voltage_chan('Dev1/ao1','mychanel',0,3.3)
             self.hilo.start()
 
 
-    def Excel(self,a=False,x=None):
+    def Excel(self,a=False,x=None,y=None):
         self.runS = a
         count = 1
         self.value_a = [0,0,0,0]
-        a = [0,0,0,0]
+        arr = [0,0,0,0]
         c=0
         self.value = self.task.read()
         self.r += 1 
-        while (self.runS) and (count < 30):
+        print(x)
+        while (self.runS) and (count < 100):
+            #time.sleep(0.1)
             self.value = self.task.read()
-            #self.task2.write(self.value[2]+0.07)
+            #self.task2.write(2.5)
             self.task2.write((2.49E-16 + 2.8*int(x) + 9.79E-14*int(x)**2 + -3.77E-13*int(x)**3 + 6.86E-13*int(x)**4 + -5.88E-13*int(x)**5 + 1.91E-13*int(x)**6)/100)
+            self.task3.write((2.49E-16 + 2.8*int(y) + 9.79E-14*int(y)**2 + -3.77E-13*int(y)**3 + 6.86E-13*int(y)**4 + -5.88E-13*int(y)**5 + 1.91E-13*int(y)**6)/100)
             #self.task2.write((2.49E-16 + 2.8*int(c) + 9.79E-14*int(c)**2 + -3.77E-13*int(c)**3 + 6.86E-13*int(c)**4 + -5.88E-13*int(c)**5 + 1.91E-13*int(c)**6)/100)
             #print(self.value[2]+0.07)    
             count +=1
@@ -49,19 +54,29 @@ class Testing():
             self.value_a[1] = self.value[1] + self.value_a[1]
             self.value_a[2] = self.value[2] + self.value_a[2]
             self.value_a[3] = self.value[3] + self.value_a[3]
-            #print(self.value_a[2])
+            #print(self.value[3])
             #self.celdaI = f'A{self.count}'
             #self.celdaD = f'B{self.count}'
             #self.ws[self.celdaI] = self.value[0]
-            #self.ws[self.celdaD] = self.value[1]
-        a[0] = (self.value_a[0])/count
-        a[1] = (self.value_a[1])/count
-        a[2] = (self.value_a[2])/count
-        a[3] = (self.value_a[3])/count  
+            #self.ws[self.celdaD] = self.value[1] 
+        arr[0] = (self.value_a[0])/count #Galga 1
+        arr[1] = (self.value_a[1])/count #Galga 2 
+        arr[2] = (self.value_a[2])/count #Corriente 1
+        arr[3] = (self.value_a[3])/count  #Corriente 2
         self.i1 = f'A{self.r}'
-        self.ws[self.i1] = self.value[2]
-        print(a[2]+0.07)      
-       # print(f"CELDA 1   CELDA 2   i1   i2 \n {a[0]:.4f}   {a[1]:.4f}  {a[2]:.4f} {a[3]:.4f}")
+        self.i2 = f'B{self.r}'
+        self.F1 = f'C{self.r}'
+        self.F2 = f'D{self.r}'
+        F1 = 25000*arr[0] - 0.025
+        F2 = 25000*arr[1] - 0.025
+        I1 = -9668.5*(arr[2])**6 + 11067*(arr[2])**5 -4938.8*(arr[2])**4 + 1078.1*(arr[2])**3 -117.83*(arr[2])**2 + 8.7677*(arr[2]) - 0.0427#porfavor pruebe la función
+        I2 = -9668.5*(arr[3])**6 + 11067*(arr[3])**5 -4938.8*(arr[3])**4 + 1078.1*(arr[3])**3 -117.83*(arr[3])**2 + 8.7677*(arr[3]) - 0.0427#porfavor pruebe la función
+        self.ws[self.i1] = "{:.6f}".format(I1)
+        self.ws[self.i2] = "{:.6f}".format(I2)
+        self.ws[self.F1] = "{:.6f}".format(F1)
+        self.ws[self.F2] = "{:.6f}".format(F2)      
+        print(f"\t CELDA 1   CELDA 2   i1   i2 \n \
+        {arr[0]:.6f}   {arr[1]:.6f}  {arr[2]:.6f} {arr[3]:.6f}")
         self.wb.save('calibra.xlsx')
     
     def Salir(self):
@@ -89,16 +104,24 @@ class Ventana(Frame,Testing):
         mac_sup.grid(row=0, columnspan=12, rowspan=10, padx= 5, pady = 5)
         Label(mac_sup, text = "ciclo de trabajo").grid(row=0, column=0)
         self.ciclo = StringVar()
+        self.c_val = StringVar(value=1)
+        self.c2_val = StringVar(value=1)
         #print(self.ciclo.get())
         Entry(mac_sup, textvariable=self.ciclo).grid(row=0, column=1, padx=7, pady=2)
-        Label(mac_sup, text='Baudios:').grid(row=0, column=2)
-        self.baud = Spinbox(mac_sup, from_=1, to=115200, width=7, values=115200)
+        Label(mac_sup, text='Bobina 1:').grid(row=0, column=2)
+        self.baud = Spinbox(mac_sup, from_=0, to=100, width=7, values=arange(0,101),textvariable=self.c_val)
         self.baud.grid(row=0, column=3)
+        Label(mac_sup, text='Bobina 2:').grid(row=1, column=2)
+        self.baud2 = Spinbox(mac_sup, from_=0, to=100, width=7, values=arange(0,101),textvariable=self.c2_val)
+        self.baud2.grid(row=1, column=3)
+        
 
 
 
     def registrar(self):
-        self.analog.Excel(True,self.ciclo.get())
+        self.analog.Excel(True,self.c_val.get(),self.c2_val.get())
+        
+        #self.analog.Excel(True,self.ciclo.get())
 
     
 
--- main.py
+++ main.py
@@ -17,10 +17,13 @@ ws = wb.active
 
 task = nidaqmx.Task()
 task2 = nidaqmx.Task()
-task.ai_channels.add_ai_voltage_chan('Dev1/ai0',min_val= 0, max_val = 5)
-task.ai_channels.add_ai_voltage_chan('Dev1/ai1',min_val= 0, max_val = 5)
+task3 = nidaqmx.Task()
+task.ai_channels.add_ai_voltage_chan('Dev1/ai0',min_val= 0, max_val = 6)
+task.ai_channels.add_ai_voltage_chan('Dev1/ai1',min_val= 0, max_val = 6)
 task.ai_channels.add_ai_voltage_chan('Dev1/ai2',min_val= 0, max_val = 6)
-task2.ao_channels.add_ao_voltage_chan('Dev1/ao1','mychanel',0,6)
+task.ai_channels.add_ai_voltage_chan('Dev1/ai3',min_val= 0, max_val = 6)
+task3.ao_channels.add_ao_voltage_chan('Dev1/ao0','mychanel',0,3.3)
+task2.ao_channels.add_ao_voltage_chan('Dev1/ao1','mychanel',0,3.3)
 #task.write(value)
 #time.sleep(2)
 value2 = 0
@@ -30,13 +33,14 @@ for count in range(1,10001,1):
     time.sleep(0.1)
     #print(count)
     value = task.read()
-    task2.write(value[2]+0.07)
-    print(value[2]+0.07)
+    #task2.write(0.105)
+    #task3.write(0.2)
+    print(f'{value[2]} {value[3]}')
     #celdaI = f'A{count}'
     #celdaD = f'B{count}'
     #ws[celdaI] = value[0]
     #ws[celdaD] = value[1]
-wb.save('Datos.xlsx')
+#wb.save('Datos.xlsx')
 
 #value = 0.5
 #task.write(value)
